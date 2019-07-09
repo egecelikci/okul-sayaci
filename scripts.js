6c9ff3b4339fd828b10e9266203b33165eeb26a5
egecue <egecelikci@gmail.com>
2019-07-09 19:56:47 +0300

    Fixed dates.

--- scripts.js
+++ scripts.js
@@ -4,32 +4,31 @@ window.onload = function onLoad() {
         trailColor: '#646464'
     });
 
+    $.getJSON("https://egecue.github.io/veriler/okul-sayaci.json", function(data) {
+        function progress() {
+            var now = new Date();
+            var start = new Date(data.start);
+            var end = new Date(data.end);
+            var done = (now - start) / (end - start);
+            var distance = (end - now);
+            var _second = 1000;
+            var _minute = _second * 60;
+            var _hour = _minute * 60;
+            var _day = _hour * 24;
+            var days = Math.floor(distance / _day);
+            var hours = Math.floor((distance % _day) / _hour);
+            var minutes = Math.floor((distance % _hour) / _minute);
+            var seconds = Math.floor((distance % _minute) / _second);
+            document.getElementById("percent").innerHTML = done * 100 + "%";
+            document.getElementById("countdown").innerHTML = `${days} gün, ${hours} saat, ${minutes} dakika, ${seconds} saniye.`;
+            return done;
+        }
+        line.animate(progress()); // Number from 0.0 to 1.0
 
-    function progress() {
-        var now = new Date();
-        var start = new Date(2018, 8, 17);
-        var end = new Date(2019, 5, 14);
-        var done = (now - start) / (end - start);
-        var distance = (end - now);
-        var _second = 1000;
-        var _minute = _second * 60;
-        var _hour = _minute * 60;
-        var _day = _hour * 24;
-        var days = Math.floor(distance / _day);
-        var hours = Math.floor((distance % _day) / _hour);
-        var minutes = Math.floor((distance % _hour) / _minute);
-        var seconds = Math.floor((distance % _minute) / _second);
-        document.getElementById("percent").innerHTML = done * 100 + "%";
-        document.getElementById("countdown").innerHTML = `${days} gün, ${hours} saat, ${minutes} dakika, ${seconds} saniye.`;
-        return done;
-    }
+        setInterval(update, 1000);
 
-    line.animate(progress()); // Number from 0.0 to 1.0
-
-    setInterval(update, 1000);
-
-    function update() {
-        line.set(progress());
-    }
-
+        function update() {
+            line.set(progress());
+        }
+    });
 };
